e19670e6527f4319fb4f97383a1fc4eb0d52e39b
gowtham <gowtham3798@gmail.com>
2022-04-07 00:57:35 +0530

    third commit

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import useState from 'react'
 
 function App() {
-  return (
-    <div>
-      <Phone />
-    </div>
+  
+  const [mobiles,setMobiles] = useState([])
+   
+  fetch("http://localhost:4000/mobiles")
+  .then((data) => data.json())
+  .then((mbs) => setMobiles(mbs))
 
+  return (
+    <div className="App phone-list-container">
+      {mobiles.map(mobile =><Phone mobile={mobile} /> )}
+    </div>
   );
 }
 
- function Phone(){
-  const mobile = {
-    model: "OnePlus 9 5G",
-    img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-    company: "Oneplus"
-  };
+ function Phone({mobile}){
   return(
     <div className="phone-container">
-      <img src={mobile.img} alt="" className="phone-picture"/>
+      <img src={mobile.img} alt={mobile.model} className="phone-picture"/>
       <h2 className="phone-name">{mobile.model}</h2>
       <p className="phone-company">{mobile.company}</p>
     </div>
