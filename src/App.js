5eb86931241ff34408f079eef4e323487cd04c09
gowtham <gowtham3798@gmail.com>
2022-04-08 01:30:21 +0530

    checkout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,177 @@
-import logo from './logo.svg';
 import './App.css';
-import useState from 'react'
+import  {createContext,useState, useEffect,useContext } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+import {useNavigate,Route,Routes,Navigate} from "react-router-dom";
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+
+// const API = 'https://e-commerce46.herokuapp.com'
+const API = 'http://localhost:5000'
+
+const cartCtx = createContext();
+
+const currencyFormatter = (number) => 
+      new Intl.NumberFormat('en-IN' , {style : 'currency' , currency : "INR"}).format(number);
 
 function App() {
-  
-  const [mobiles,setMobiles] = useState([])
-   
-  fetch("http://localhost:4000/mobiles")
-  .then((data) => data.json())
-  .then((mbs) => setMobiles(mbs))
+  const navigate = useNavigate()
+  const [cart,setCart] = useState([])
 
-  return (
+  useEffect(() =>{
+    fetch(`${API}/cart`)
+    .then(data => data.json())
+    .then((cartmbs) => setCart(cartmbs))
+    },[])
+
+  const [mobiles,setMobiles] = useState([]);
+   useEffect(() =>{
+    fetch(`${API}/mobiles`)
+    .then((data) => data.json())
+    .then((mbs) => setMobiles(mbs))
+  },[])
+
+  const updateCart = ({mobile,action}) => {
+   
+    fetch(`${API}/cart?type=${action}`,{
+      method:"PUT",
+      body:JSON.stringify(mobile),
+      headers:{
+        "Content-Type" : "application/json"
+      }
+    })
+    .then(data => data.json())
+    .then((latestCart) => setCart(latestCart))
+  }
+
+  const totalQty = cart
+  .map((item) => item.qty)
+  .reduce((sum,qty) => sum+qty,0)
+  
+return (
     <div className="App phone-list-container">
-      {mobiles.map(mobile =><Phone mobile={mobile} /> )}
+      <cartCtx.Provider value={[cart,updateCart,setCart]}>
+
+       <AppBar position="static">
+       <Toolbar>
+         <Button  color='inherit' onClick={() => navigate('/')}>Home</Button>
+       <IconButton color='inherit' aria-label='Cart' style={{marginLeft : 'auto'}} onClick={() => navigate('/cart')} >
+       <Badge badgeContent={totalQty} color="secondary">
+         <ShoppingCartIcon />
+       </Badge>
+       </IconButton>
+       </Toolbar>
+       </AppBar>
+
+       <Routes>
+         <Route exact path='/' element={mobiles.map(mobile =>
+      <Phone key={mobile._id} mobile={mobile} /> )} />
+
+      <Route exact path='/cart' element={<Cart />} />
+       </Routes>
+      {/* {mobiles.map(mobile =>
+      <Phone key={mobile._id} mobile={mobile} /> )} */}
+     
+      </cartCtx.Provider>
     </div>
   );
 }
 
+function Cart(){
+  const [cart,updateCart,setCart] = useContext(cartCtx)
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  useEffect(() =>{
+    fetch(`${API}/cart`)
+    .then(data => data.json())
+    .then((cartmbs) => setCart(cartmbs))
+    },[])
+
+    const checkoutCart = () => {
+   
+      fetch(`${API}/checkout`,{
+        method:"POST",
+        body:JSON.stringify(cart),
+        headers:{
+          "Content-Type" : "application/json"
+        }
+      })
+      .then(data => data.json())
+      .then((latestCart) => setCart(latestCart))
+      .then(() => setOpen(true))
+      .then(() => setTimeout(() => navigate('/'),1000));
+    }
+
+  const total = cart
+  .map((item) => item.price * item.qty)
+  .reduce((sum,item) => sum+item,0)
+  return(
+    <section className='cart-list'>
+      <h1>Purchase Items</h1>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+  <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+    This is a success message!
+  </Alert>
+</Snackbar>
+    <div className="cart-list-container">
+      {cart.map((mobile)=>(
+        <CartItem key={mobile._id} mobile={mobile} />
+      ))}
+    </div>
+    <div className='cart-checkout'>
+    <h2 className='cart-checkout-price'>{currencyFormatter(total)}</h2>
+      <button onClick={() => checkoutCart()}>Checkout</button>
+    </div>
+    </section>
+  )
+}
+
+function CartItem({mobile}){
+  const [cart,updateCart] = useContext(cartCtx)
+  return(
+    <div className="cart-item-container">
+      <img src={mobile.img} alt={mobile.model} className="cart-item-picture"/>
+      <div>
+      <h2 className="cart-item-name">{mobile.model}</h2>
+      <p className="cart-item-company">{mobile.company}</p>
+      {/* <h2 className="cart-item-price">{currencyFormatter(mobile.price)}</h2> */}
+      <p className="cart-item-quantity">
+         <button onClick={() => updateCart({mobile,action:'decrement'})}>➖</button> {mobile.qty} <button onClick={() => updateCart({mobile,action:'increment'})}>➕</button> 
+      </p>
+       </div>
+       <h2 className="cart-item-price">{currencyFormatter(mobile.price*mobile.qty)}</h2>
+    </div>
+  )
+}
+
  function Phone({mobile}){
+  const [cart,updateCart] = useContext(cartCtx)
   return(
     <div className="phone-container">
       <img src={mobile.img} alt={mobile.model} className="phone-picture"/>
       <h2 className="phone-name">{mobile.model}</h2>
       <p className="phone-company">{mobile.company}</p>
+      <h2 className="phone-price">{currencyFormatter(mobile.price)}</h2>
+      <button
+      onClick={() => updateCart({mobile,action:'increment'})}
+      >Add to cart</button>
     </div>
   )
 }
